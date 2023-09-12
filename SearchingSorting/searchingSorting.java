a33c522602cc74a8107dd429f51cc912464cf92f
Anmol Wassan <86876090+AnmolWasson@users.noreply.github.com>
2023-09-12 15:06:09 +0530

    ammend

--- SearchingSorting/searchingSorting.java
+++ SearchingSorting/searchingSorting.java
@@ -1,9 +1,16 @@
 package SearchingSorting;
+
 import java.util.*;
 
 public class searchingSorting {
+    static Scanner sc = new Scanner(System.in);
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+       
+         ques1();
+    }
+
+    static void  ques1() {
+
         int n = sc.nextInt();
         int m = sc.nextInt();
         int k = sc.nextInt();
@@ -19,51 +26,38 @@ public class searchingSorting {
         }
 
         Arrays.sort(arr2);
-        // Arrays.sort(arr1);
-        int ans = helpr(arr1, arr2, k);
-        System.out.println(ans);
+        Arrays.sort(arr1);
+
+       System.out.println(helpr(arr1,arr2,k));
     }
 
-    static int helpr(int[] arr1, int[] arr2, int k) {
-         
-        int n = arr1.length, count = 0,m=arr2.length;
-        boolean[] vis = new boolean[m];
-        for (int i = 0; i < n; i++) {
 
-            if (bs(arr1[i], arr2, k,vis)) {
-              
+    static int helpr(int[]arr1,int []arr2,int k) {
+            int n =arr1.length,m =arr2.length;
+            int i=0,j=0,count=0; 
+        while(i<n&&j<m){
+            
+            int left =arr1[i]-k,right =arr1[i]+k;
+
+            if(left<=arr2[j]&&right>=arr2[j]){
+
+                i++;
+                j++;
                 count++;
             }
 
+            else if(arr1[i]>arr2[j]){
+                j++;
+            }
+
+            else{
+               i++;
+            }
         }
+
         return count;
     }
 
-    static boolean bs(int a, int[] arr, int k,boolean[]vis) {
-
-        int lo = 0, hi = arr.length - 1,mod=(int)1e9+7;
-
-        while (lo < hi) {
-
-            int mid = lo + (hi - lo) / 2;
-            if (!vis[mid]&&(arr[mid] >= (a - k)%mod && arr[mid] <= (a + k)%mod)) {
-                  
-                 hi = mid ;
-            }  else if(arr[mid]<(a-k)%mod){
-               lo=mid+1;
-            }
-            else{
-                hi=mid;
-            }
-
-        }
-         
-        if(hi>=0&&!vis[hi]&&arr[hi] >= a - k && arr[hi] <= a + k){
-            vis[hi]=true;
-            return true;
-        }
-
-        return false;
-    }
+       
 
 }
